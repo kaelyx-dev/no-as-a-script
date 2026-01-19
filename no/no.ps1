b72272e719986fadbdb6e09a5b787d7fadd64c4f
Kaelyx <43659841+kaelyx-dev@users.noreply.github.com>
2026-01-19 15:00:21 +0000

    Add PowerShell script to invoke REST method and speak response

---
+++ no/no.ps1
@@ -0,0 +1,12 @@
+$resp = try { 
+  Invoke-RestMethod https://naas.isalman.dev/no 
+} catch { 
+  $_.Exception.Response
+  exit 1
+}
+
+echo $resp.reason
+Set-Clipboard -value $resp.reason
+
+$sp = New-Object -ComObject SAPI.SpVoice
+$sp.Speak($resp.reason)
